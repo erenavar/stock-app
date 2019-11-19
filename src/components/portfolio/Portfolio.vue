037affdd3f56be54340b157e58065ab05b0fbef2
erenavar <43113599+erenavar@users.noreply.github.com>
2019-11-19 18:52:57 +0300

    Initial commit

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Stock from "./Stock.vue";
+
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: "stockPortfolio" // portfolio data
+    })
+  },
+  components: {
+    appStock: Stock
+  }
+};
+</script>
